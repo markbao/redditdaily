5483d04a10b83de97a38e5db300b4445bb732234
Mark Bao <mark@markbao.com>
2015-12-14 04:27:11 -0500

    Renamed redditdaily_cron.go

--- redditdaily.go
+++ redditdaily.go
@@ -1,6 +1,30 @@
 package main
-import "github.com/markbao/redditdaily/redditdaily"
+
+import (
+	"github.com/robfig/cron"
+	"github.com/spf13/viper"
+	"github.com/markbao/redditdaily/redditdaily"
+	"fmt"
+)
 
 func main() {
-	redditdaily.Run()
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("../")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	c := cron.New();
+	error := c.AddFunc(fmt.Sprintf("0 %v %v * * *", viper.GetInt("cron_min"), viper.GetInt("cron_hour")), redditdaily.Run)
+	if error != nil {
+		fmt.Println(error)
+	}
+	c.Start();
+
+	fmt.Println("Cron started.")
+
+	select{}
 }
